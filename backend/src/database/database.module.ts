30e44b0d63322f0c2f62c8811d2da93761870ed4
yuuki-hyn <yuhyn1535@gmail.com>
2026-09-23 15:31:02 +0900

    feat: DatabaseModuleでpg.Poolを提供

--- backend/src/database/database.module.ts
+++ backend/src/database/database.module.ts
@@ -1,4 +1,27 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { Pool } from 'pg';
 
-@Module({})
+export const PG_POOL = 'PG_POOL';
+
+@Global()
+@Module({
+    imports: [ConfigModule],
+    providers: [
+        {
+            provide: PG_POOL,
+            inject: [ConfigService],
+            useFactory: (configService: ConfigService) => {
+                return new Pool({
+                    host: configService.get<string>('DB_HOST'),
+                    port: configService.get<number>('DB_PORT'),
+                    user: configService.get<string>('DB_USER'),
+                    password: configService.get<string>('DB_PASSWORD'),
+                    database: configService.get<string>('DB_NAME'),
+                });
+            },
+        },
+    ],
+    exports: [PG_POOL],
+})
 export class DatabaseModule {}
